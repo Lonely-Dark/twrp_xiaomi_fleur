f9f5a9cf49c853b3516dbf8654da79e9b4064cbb
takumi021 <omrameshpatel@gmail.com>
2022-04-05 13:13:33 +0000

    TWRP-ify

--- twrp_fleur.mk
+++ twrp_fleur.mk
@@ -13,13 +13,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # Inherit from fleur device
 $(call inherit-product, device/xiaomi/fleur/device.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
-$(call inherit-product, vendor/omni/config/gsm.mk)
+# Inherit some common TWRP stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/twrp/config/gsm.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := fleur
-PRODUCT_NAME := omni_fleur
+PRODUCT_NAME := twrp_fleur
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := fleur
 PRODUCT_MANUFACTURER := xiaomi
